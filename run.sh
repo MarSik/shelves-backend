babc009f26d945126aaa4129ee7b8a839bcfbb87
Martin Sivak <msivak@redhat.com>
2016-03-25 23:50:55 +0100

    Allow configuration of memcached server and use the proper dir for data

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-export DIR_CONFIG="/etc/shelves"
-export DEPLOY="/server.war"
-export LOG_FILE="/shelves/log/shelves.log"
+DIR_CONFIG="/etc/shelves"
+DEPLOY="/server.war"
+LOG_FILE="/shelves/log/shelves.log"
+DIR_DATA="/shelves/data"
 
 umask 0077
 mkdir -m 0700 -p $DIR_DATA
@@ -10,5 +11,13 @@ mkdir -m 0700 -p $DIR_DATA
 JAVA_OPTS="-Xms128m -Xmx3072m -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=127.0.0.1:8000,suspend=n"
 MAINCLASS=$(grep "Main-Class: " "$DEPLOY/META-INF/MANIFEST.MF" | cut -d ' ' -f2 | sed -e 's/[[:space:]]//g')
 
-exec java $JAVA_OPTS -cp "$DEPLOY" -Dlogging.config=$DIR_CONFIG/logback.xml -Ddebug -Dserver.port=8080 -Dspring.config.location=$DIR_CONFIG/ $MAINCLASS "$@"
+exec java $JAVA_OPTS -cp "$DEPLOY" \
+-Dlogging.config=$DIR_CONFIG/logback.xml \
+-Ddebug \
+-Dserver.port=8080 \
+-Dspring.config.location=$DIR_CONFIG/ \
+-Dstorage.path=$DIR_DATA \
+"-Dshelves.memcached.server=$MEMCACHED" \
+"-Dspring.datasource.url=jdbc:mysql://$MYSQL_SERVER/$MYSQL_DATABASE?useUnicode=yes&characterEncoding=UTF-8" \
+$MAINCLASS "$@"
 
