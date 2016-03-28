4b9661591fe88a67796c26e17b3c2708d8e3be31
Martin Sivak <msivak@redhat.com>
2016-03-28 22:19:15 +0200

    Improve configurability of the container

--- run.sh
+++ run.sh
@@ -2,13 +2,18 @@
 
 DIR_CONFIG="/etc/shelves"
 DEPLOY="/server.war"
-LOG_FILE="/shelves/log/shelves.log"
+DIR_LOG="/shelves/log"
+LOG_FILE="$DIR_LOG/shelves.log"
 DIR_DATA="/shelves/data"
+
+umask 0077
+mkdir -m 0700 -p $DIR_LOG
+mkdir -m 0700 -p $DIR_DATA
 
 umask 0077
 mkdir -m 0700 -p $DIR_DATA
 
-JAVA_OPTS="-Xms$JAVA_MIN_MEM -Xmx$JAVA_MAX_MEM -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=127.0.0.1:8000,suspend=n"
+JAVA_OPTS="-Xms$JAVA_MIN_MEM -Xmx$JAVA_MAX_MEM -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=0.0.0.0:8000,suspend=n"
 MAINCLASS=$(grep "Main-Class: " "$DEPLOY/META-INF/MANIFEST.MF" | cut -d ' ' -f2 | sed -e 's/[[:space:]]//g')
 
 exec java $JAVA_OPTS -cp "$DEPLOY" \
@@ -17,7 +22,7 @@ exec java $JAVA_OPTS -cp "$DEPLOY" \
 -Dserver.port=8080 \
 -Dspring.config.location=$DIR_CONFIG/ \
 -Dstorage.path=$DIR_DATA \
-"-Dshelves.memcached.server=$MEMCACHED" \
+"-Dshelves.memcached.server=$MEMCACHED_SERVER" \
 "-Dspring.datasource.url=jdbc:mysql://$MYSQL_SERVER/$MYSQL_DATABASE?useUnicode=yes&characterEncoding=UTF-8" \
 $MAINCLASS "$@"
 
